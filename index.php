dfbd9054d175e242f19357f7eed4ebc6a2b6386f
Luddifee <ludwig@ludwig-lehnert.de>
2021-02-08 18:49:36 +0100

    update index.php

--- index.php
+++ index.php
@@ -11,6 +11,21 @@
     <?php include 'db.php'; ?>
     <p><?php
             echo $_SERVER['HTTP_HOST'];
+            $servername = $_SERVER['HTTP_HOST'];
+            $username = "root";
+            $password = "123456";
+            $dbname = "inordinatus";
+
+            $conn = new mysqli($servername, $username, $password, $dbname);
+
+            if ($conn->connect_error) {
+                echo "Connection failed: " . $conn->connect_error;
+                die("Connection failed: " . $conn->connect_error);
+            }
+
+            echo "Connection established!";
+
+            mysqli_close($conn);
         ?></p>
 </body>
 </html>
