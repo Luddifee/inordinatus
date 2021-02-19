2bdcbd7dbb2ebfc5810a1fb825bc2fd0513f57da
Luddifee <ludwig@ludwig-lehnert.de>
2021-02-19 17:24:32 +0100

    introduced working tokens

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const PORT  = 8080;
-const TOKEN_TTL = 1440;
+const TOKEN_LENGTH = 64;
+const TOKEN_TTL_MINUTES = 60;
 const BCRYPT_SALT_ROUNDS = 10;
 const BCRYPT_TOKEN_SALT_ROUNDS = 5;
+
+const RAND_SYMBOLS = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!\"§$%&/()=?{}[]-_.:+*~#<>';
 
 const USERS_FILE = './data/users.json';
 const TOKEN_FILE = './data/tokens.json';
@@ -14,12 +17,11 @@ const express = require('express');
 const process = require('process');
 const crypto = require('crypto');
 
-
 // LOGGING
 function log(message) {
     const d = new Date();
     const log_path = 'logs/'+d.getDay()+'-'+d.getMonth()+'-'+d.getFullYear()+'.log';
-    const log_message = '['+d.getHours()+':'+d.getMinutes()+':'+(d.getSeconds()<10?'0':'')+ d.getSeconds()+'] '+message;
+    const log_message = '['+(d.getHours()<10?'0':'')+d.getHours()+':'+(d.getMinutes()<10?'0':'')+d.getMinutes()+':'+(d.getSeconds()<10?'0':'')+ d.getSeconds()+'] '+message;
     console.log(log_message);
     fs.appendFileSync(log_path, log_message+'\n');
 }
@@ -33,6 +35,13 @@ function setup_fs() {
 setup_fs();
 
 // USEFUL FUNCTIONS
+function random_string(length) {
+    var result = '';
+    for (var i = 0; i < length; i++)
+        result += RAND_SYMBOLS[crypto.randomInt(RAND_SYMBOLS.length)]
+    return result;
+}
+
 function hashSync(string, rounds=BCRYPT_SALT_ROUNDS) {
     return bcrypt.hashSync(string, bcrypt.genSaltSync(rounds));
 }
@@ -88,6 +97,10 @@ function get_users() {
     return JSON.parse(fs.readFileSync(USERS_FILE));
 }
 
+function get_user(username) {
+    return get_users().find(user => user.username === username);
+}
+
 function add_user(user) {
     const users = get_users();
     if (users.find(user => user.username === username) !== undefined)
@@ -96,32 +109,51 @@ function add_user(user) {
     fs.writeFileSync(USERS_FILE, JSON.stringify(users));
 }
 
-function valid_login(username, password) {
+function login_valid(username, password) {
     const user = get_users().find(user => user.username === username);
     return user !== undefined && bcrypt.compareSync(password, user.password);
 }
 
+// TOKENS
 function get_tokens() {
     if(!fs.existsSync(TOKEN_FILE)) return [];
     return JSON.parse(fs.readFileSync(TOKEN_FILE));
 }
 
-function valid_token(username, token) {
-    const tokens = get_tokens();
-    console.log(tokens);
-    return tokens.find(t => bcrypt.compareSync(token, t["token"])) !== undefined;
+function token_cleanup(tokens) {
+    const result = [];
+    const time = new Date().getTime();
+    for (var i = 0; i < tokens.length; i++)
+        if ((time - tokens[i].time) / (60 * 1000) < TOKEN_TTL_MINUTES)
+            result.push(tokens[i]);
+    tokens.length = 0;
+    tokens.push(...result);
 }
 
-function add_token(username) {
-    const tokenString = String.fromCharCode(...crypto.randomBytes(64));
+function token_valid(token) {
+    const tokens = get_tokens();
+    token_cleanup(tokens);
+    fs.writeFileSync(TOKEN_FILE, JSON.stringify(tokens));
+    for (var i = 0; i < tokens.length; i++)
+        if (bcrypt.compareSync(token, tokens[i]["token"]))
+            return true;
+    return false;
+}
+
+function gen_token(username) {
+    if (get_user(username) === undefined) return 1;
+    const tokenString = random_string(TOKEN_LENGTH);
     const token = {
         token: hashSync(tokenString, BCRYPT_TOKEN_SALT_ROUNDS),
         username: username,
         time: new Date().getTime()
     };
     const tokens = get_tokens();
+    const duplicate = tokens.find(t => t.username === username);
+    if (duplicate !== undefined)
+        tokens.splice(tokens.indexOf(duplicate), 1);
     tokens.push(token);
-    fs.writeFileSync(TOKEN_FILE, JSON.stringify(tokens))
+    fs.writeFileSync(TOKEN_FILE, JSON.stringify(tokens));
     return tokenString;
 }
 
@@ -148,6 +180,11 @@ function request_valid(request, params) {
     return true;
 }
 
+
+console.log(gen_token('Benutzername1'));
+
+
+// EXPRESS
 const app = express();
 
 app.use(express.json());
@@ -157,11 +194,13 @@ app.use(express.static('html/'));
 app.post('/api/login', async (req, res) => {
     log(req.ip+' POST /api/login');
     var result_code = 0;
+    var token = undefined;
     if(!request_valid(req, ["username", "password"]))
         result_code = 1;
-    else if(!valid_login(req.body["username"], req.body["password"]))
+    else if(!login_valid(req.body["username"], req.body["password"]))
         result_code = 10;
-    res.send({token:"not implemented yet", resultCode:result_code});
+    else token = gen_token(req.body["username"]);
+    res.send({token:token, resultCode:result_code});
 });
 
 app.post('/api/logout', async (req, res) => {
@@ -198,13 +237,13 @@ app.put('/api/users', async (req, res) => {
 
 // TOKEN
 app.post('/api/token', async (req, res) => {
-    log(req.ip+' PUT /api/users');
+    log(req.ip+' POST /api/token');
     var result_code = 0;
     if(req.body["token"] === undefined)
         result_code = 1;
-    else if(!valid_token(req.body["token"]))
+    else if(!token_valid(req.body["token"]))
         result_code = 10;
-    res.send({value:"not implemented yet", resultCode: result_code});
+    res.send({resultCode: result_code});
 });
 
 app.get('*', (req, res) => res.redirect('/'))
